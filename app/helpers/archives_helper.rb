b63113eecd8204f8387b5a8f74373bf745bda3a8
Drastyn <sheizck@gmail.com>
2021-06-11 13:17:37 -0400

    Change in the response

--- app/helpers/archives_helper.rb
+++ app/helpers/archives_helper.rb
@@ -3,7 +3,7 @@ module ArchivesHelper
 
   def set_response
     render json: { 
-      data: ActiveModel::Serializer::CollectionSerializer.new(@archives, each_serializer: ArchiveSerializer),
+      images: ActiveModel::Serializer::CollectionSerializer.new(@archives, each_serializer: ArchiveSerializer),
       next_page_url: @archives.next_page,
       prev_page_url: @archives.prev_page
     }
